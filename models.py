caf40b49d9777f2164c2edf93e44908b819476f6
Jonna Matthiesen <matthiesenjonna@gmail.com>
2023-04-26 14:45:40 +0200

    Add option to train mobilenetv2

--- models.py
+++ models.py
@@ -34,7 +34,7 @@ import torch.nn.init as init
 
 from torch.autograd import Variable
 
-__all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
+__all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202', 'mobilenetv2']
 
 def _weights_init(m):
     classname = m.__class__.__name__
@@ -116,6 +116,82 @@ class ResNet(nn.Module):
         return out
     
 
+class MobileNetV2(nn.Module):
+    def __init__(self, output_size=10, alpha = 1):
+        super(MobileNetV2, self).__init__()
+        self.output_size = output_size
+
+        # first conv layer 
+        self.conv0 = nn.Conv2d(3, int(32*alpha), kernel_size = 3, stride = 1, padding = 1, bias = False)
+        self.bn0 = nn.BatchNorm2d(int(32*alpha))
+
+        # build bottlenecks
+        BaseBlock.alpha = alpha
+        self.bottlenecks = nn.Sequential(
+            BaseBlock(32, 16, t = 1, downsample = False),
+            BaseBlock(16, 24, downsample = False),
+            BaseBlock(24, 24),
+            BaseBlock(24, 32, downsample = False),
+            BaseBlock(32, 32),
+            BaseBlock(32, 32),
+            BaseBlock(32, 64, downsample = True),
+            BaseBlock(64, 64),
+            BaseBlock(64, 64),
+            BaseBlock(64, 64),
+            BaseBlock(64, 96, downsample = False),
+            BaseBlock(96, 96),
+            BaseBlock(96, 96),
+            BaseBlock(96, 160, downsample = True),
+            BaseBlock(160, 160),
+            BaseBlock(160, 160),
+            BaseBlock(160, 320, downsample = False))
+
+        # last conv layers and fc layer
+        self.conv1 = nn.Conv2d(int(320*alpha), 1280, kernel_size = 1, bias = False)
+        self.bn1 = nn.BatchNorm2d(1280)
+        self.avg = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(1280, output_size)
+
+        self.apply(_weights_init)
+
+        # weights init
+        # self.weights_init()
+
+
+    def weights_init(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
+    def forward(self, inputs):
+
+        # first conv layer
+        x = F.relu6(self.bn0(self.conv0(inputs)), inplace = True)
+        # assert x.shape[1:] == torch.Size([32, 32, 32])
+
+        # bottlenecks
+        x = self.bottlenecks(x)
+        # assert x.shape[1:] == torch.Size([320, 8, 8])
+
+        # last conv layer
+        x = F.relu6(self.bn1(self.conv1(x)), inplace = True)
+        # assert x.shape[1:] == torch.Size([1280,8,8])
+
+        # global pooling and fc (in place of conv 1x1 in paper)
+        x = self.avg(x)
+        x = torch.flatten(x, start_dim=1, end_dim=-1)
+
+        x = self.fc(x)
+
+        return x
+    
+
 def resnet20():
     return ResNet(BasicBlock, [3, 3, 3])
 
@@ -139,6 +215,9 @@ def resnet110():
 def resnet1202():
     return ResNet(BasicBlock, [200, 200, 200])
 
+def mobilenetv2():
+    return MobileNetV2()
+
 
 def test(net):
     import numpy as np
@@ -156,3 +235,16 @@ if __name__ == "__main__":
             print(net_name)
             test(globals()[net_name]())
             print()
+
+
+
+
+# encoding: utf-8
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+from blocks import BaseBlock
+
