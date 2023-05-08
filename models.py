15c8698830f09842251b77213191f08909a67f1b
Jonna Matthiesen <matthiesenjonna@gmail.com>
2023-05-08 13:59:28 +0200

    formatting

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-'''
+"""
 Properly implemented ResNet-s for CIFAR10 as described in paper [1].
 
 The implementation and structure of this file is hugely influenced by [2]
@@ -26,7 +26,7 @@ Reference:
 
 If you use this implementation in you work, please don't forget to mention the
 author, Yerlan Idelbayev.
-'''
+"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -35,7 +35,18 @@ import torch.nn as nn
 import math
 from torch.autograd import Variable
 
-__all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202', 'mobilenet', 'mobilenetv2']
+__all__ = [
+    "ResNet",
+    "resnet20",
+    "resnet32",
+    "resnet44",
+    "resnet56",
+    "resnet110",
+    "resnet1202",
+    "mobilenet",
+    "mobilenetv2",
+]
+
 
 def _weights_init(m):
     classname = m.__class__.__name__
@@ -44,6 +55,7 @@ def _weights_init(m):
     elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
         nn.init.ones_(m.weight)
         nn.init.zeros_(m.bias)
+        m.reset_running_stats()  # resets running_mewan (0), running_var (1), and num_batches tracked (0)
 
 
 class LambdaLayer(nn.Module):
@@ -52,30 +64,45 @@ class LambdaLayer(nn.Module):
         self.planes = planes
 
     def forward(self, x):
-        return F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, self.planes//4, self.planes//4), "constant", 0)
+        return F.pad(
+            x[:, :, ::2, ::2],
+            (0, 0, 0, 0, self.planes // 4, self.planes // 4),
+            "constant",
+            0,
+        )
 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, option='A'):
+    def __init__(self, in_planes, planes, stride=1, option="A"):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
-            if option == 'A':
+            if option == "A":
                 """
                 For CIFAR10 ResNet paper uses option A.
                 """
                 self.shortcut = LambdaLayer(planes)
-            elif option == 'B':
+            elif option == "B":
                 self.shortcut = nn.Sequential(
-                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-                     nn.BatchNorm2d(self.expansion * planes)
+                    nn.Conv2d(
+                        in_planes,
+                        self.expansion * planes,
+                        kernel_size=1,
+                        stride=stride,
+                        bias=False,
+                    ),
+                    nn.BatchNorm2d(self.expansion * planes),
                 )
 
     def forward(self, x):
@@ -96,13 +123,13 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        self.avg = nn.AdaptiveAvgPool2d((1,1))
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
         self.linear = nn.Linear(64, num_classes)
 
         self.apply(_weights_init)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
@@ -124,38 +151,39 @@ class ResNet(nn.Module):
 class BaseBlock(nn.Module):
     alpha = 1
 
-    def __init__(self, input_channel, output_channel, t = 6, downsample = False):
+    def __init__(self, input_channel, output_channel, t=6, downsample=False):
         """
-            t:  expansion factor, t*input_channel is channel of expansion layer
-            alpha:  width multiplier, to get thinner models
-            rho:    resolution multiplier, to get reduced representation
-        """ 
+        t:  expansion factor, t*input_channel is channel of expansion layer
+        alpha:  width multiplier, to get thinner models
+        rho:    resolution multiplier, to get reduced representation
+        """
         super(BaseBlock, self).__init__()
         self.stride = 2 if downsample else 1
         self.downsample = downsample
-        self.shortcut = (not downsample) and (input_channel == output_channel) 
+        self.shortcut = (not downsample) and (input_channel == output_channel)
 
         # apply alpha
         input_channel = int(self.alpha * input_channel)
         output_channel = int(self.alpha * output_channel)
-        
+
         # for main path:
-        c  = t * input_channel
+        c = t * input_channel
         # 1x1   point wise conv
-        self.conv1 = nn.Conv2d(input_channel, c, kernel_size = 1, bias = False)
+        self.conv1 = nn.Conv2d(input_channel, c, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(c)
         # 3x3   depth wise conv
-        self.conv2 = nn.Conv2d(c, c, kernel_size = 3, stride = self.stride, padding = 1, groups = c, bias = False)
+        self.conv2 = nn.Conv2d(
+            c, c, kernel_size=3, stride=self.stride, padding=1, groups=c, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(c)
         # 1x1   point wise conv
-        self.conv3 = nn.Conv2d(c, output_channel, kernel_size = 1, bias = False)
+        self.conv3 = nn.Conv2d(c, output_channel, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(output_channel)
-        
 
     def forward(self, inputs):
         # main path
-        x = F.relu6(self.bn1(self.conv1(inputs)), inplace = True)
-        x = F.relu6(self.bn2(self.conv2(x)), inplace = True)
+        x = F.relu6(self.bn1(self.conv1(inputs)), inplace=True)
+        x = F.relu6(self.bn2(self.conv2(x)), inplace=True)
         x = self.bn3(self.conv3(x))
 
         # shortcut path
@@ -163,49 +191,51 @@ class BaseBlock(nn.Module):
 
         return x
 
+
 class MobileNetV2(nn.Module):
-    def __init__(self, output_size=10, alpha = 1):
+    def __init__(self, output_size=10, alpha=1):
         super(MobileNetV2, self).__init__()
         self.output_size = output_size
 
-        # first conv layer 
-        self.conv0 = nn.Conv2d(3, int(32*alpha), kernel_size = 3, stride = 1, padding = 1, bias = False)
-        self.bn0 = nn.BatchNorm2d(int(32*alpha))
+        # first conv layer
+        self.conv0 = nn.Conv2d(
+            3, int(32 * alpha), kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn0 = nn.BatchNorm2d(int(32 * alpha))
 
         # build bottlenecks
         BaseBlock.alpha = alpha
         self.bottlenecks = nn.Sequential(
-            BaseBlock(32, 16, t = 1, downsample = False),
-            BaseBlock(16, 24, downsample = False),
+            BaseBlock(32, 16, t=1, downsample=False),
+            BaseBlock(16, 24, downsample=False),
             BaseBlock(24, 24),
-            BaseBlock(24, 32, downsample = False),
+            BaseBlock(24, 32, downsample=False),
             BaseBlock(32, 32),
             BaseBlock(32, 32),
-            BaseBlock(32, 64, downsample = True),
+            BaseBlock(32, 64, downsample=True),
             BaseBlock(64, 64),
             BaseBlock(64, 64),
             BaseBlock(64, 64),
-            BaseBlock(64, 96, downsample = False),
+            BaseBlock(64, 96, downsample=False),
             BaseBlock(96, 96),
             BaseBlock(96, 96),
-            BaseBlock(96, 160, downsample = True),
+            BaseBlock(96, 160, downsample=True),
             BaseBlock(160, 160),
             BaseBlock(160, 160),
-            BaseBlock(160, 320, downsample = False))
+            BaseBlock(160, 320, downsample=False),
+        )
 
         # last conv layers and fc layer
-        self.conv1 = nn.Conv2d(int(320*alpha), 1280, kernel_size = 1, bias = False)
+        self.conv1 = nn.Conv2d(int(320 * alpha), 1280, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(1280)
-        self.avg = nn.AdaptiveAvgPool2d((1,1))
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(1280, output_size)
 
         self.apply(_weights_init)
 
-
     def forward(self, inputs):
-
         # first conv layer
-        x = F.relu6(self.bn0(self.conv0(inputs)), inplace = True)
+        x = F.relu6(self.bn0(self.conv0(inputs)), inplace=True)
         # assert x.shape[1:] == torch.Size([32, 32, 32])
 
         # bottlenecks
@@ -213,7 +243,7 @@ class MobileNetV2(nn.Module):
         # assert x.shape[1:] == torch.Size([320, 8, 8])
 
         # last conv layer
-        x = F.relu6(self.bn1(self.conv1(x)), inplace = True)
+        x = F.relu6(self.bn1(self.conv1(x)), inplace=True)
         # assert x.shape[1:] == torch.Size([1280,8,8])
 
         # global pooling and fc (in place of conv 1x1 in paper)
@@ -223,7 +253,7 @@ class MobileNetV2(nn.Module):
         x = self.fc(x)
 
         return x
-    
+
 
 def resnet20():
     return ResNet(BasicBlock, [3, 3, 3])
@@ -252,23 +282,31 @@ def resnet1202():
 def mobilenetv2():
     return MobileNetV2()
 
+
 def test(net):
     import numpy as np
+
     total_params = 0
 
     for x in filter(lambda p: p.requires_grad, net.parameters()):
         total_params += np.prod(x.data.numpy().shape)
     print("Total number of params", total_params)
-    print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters()))))
+    print(
+        "Total layers",
+        len(
+            list(
+                filter(
+                    lambda p: p.requires_grad and len(p.data.size()) > 1,
+                    net.parameters(),
+                )
+            )
+        ),
+    )
 
 
 if __name__ == "__main__":
     for net_name in __all__:
-        if net_name.startswith('resnet'):
+        if net_name.startswith("resnet"):
             print(net_name)
             test(globals()[net_name]())
             print()
-
-
-
-
